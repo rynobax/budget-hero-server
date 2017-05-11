7fc4109444532cd73ab395b9adabc857f2e688a5
Ryan Baxley <rynobax@users.noreply.github.com>
2017-05-11 09:05:23 -0400

    Update response.js

--- api/response.js
+++ api/response.js
@@ -1,11 +1,5 @@
 module.exports.respond = function(res, data) {
   Promise.resolve(data)
-    .then((e) => res.json({
-      success: true,
-      data: e
-    }))
-    .catch((e) => res.json({
-      success: false,
-      error: e
-    }))
+    .then((e) => res.json(e))
+    .catch((e) => res.status(500).json(e))
 }
