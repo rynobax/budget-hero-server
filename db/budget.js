c838a43d973ff07031c1ca0ea862c7324b96de21
Ryan Baxley <rynobax@users.noreply.github.com>
2017-05-11 10:24:53 -0400

    Update budget.js

--- db/budget.js
+++ db/budget.js
@@ -1,1 +1,50 @@
+module.exports = function(db){
+  const budget = {};
+  
+  budget.getItems = function(){
+    return new Promise((resolve, reject) => {
+      db.find({}, function (err, items) {
+        if(err) reject(err);
+        else resolve(items);
+      });
+    });
+  }
 
+  budget.addItem = function(item){
+    return new Promise((resolve, reject) => {
+      db.insert(item, function (err, newItem) {
+        if(err) reject(err);
+        else resolve(newItem);
+      });
+    });
+  }
+
+  budget.updateItem = function(id, item){
+    return new Promise((resolve, reject) => {
+      db.update({_id: id}, item, {}, function (err, numReplaced) {
+        if(err) reject(err);
+        else resolve(numReplaced);
+      });
+    });
+  }
+
+  budget.deleteItem = function(id){
+    return new Promise((resolve, reject) => {
+      db.remove({_id: id}, function (err, numRemoved) {
+        if(err) reject(err);
+        else resolve(numRemoved);
+      });
+    });
+  }
+
+  budget.truncateTable = function(){
+    return new Promise((resolve, reject) => {
+      db.remove({}, function (err, numRemoved) {
+        if(err) reject(err);
+        else resolve(numRemoved);
+      });
+    });
+  }
+  
+  return budget;
+}
