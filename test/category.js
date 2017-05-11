9ffa5923891a8100c40a10be03bd0cfebb1a62d4
Ryan Baxley <rynobax@users.noreply.github.com>
2017-05-11 10:18:43 -0400

    Update category.js

--- test/category.js
+++ test/category.js
@@ -6,33 +6,35 @@ const db = require('../db');
 
 process.env.NODE_ENV = 'test';
 
-describe('Budget', () => {
+describe('Budget Category', () => {
   beforeEach((done) => {
-    db.budget.truncateTable();
+    db.category.truncateTable();
   });
-  describe('/GET budget', () => {
-    it('it should GET all the budget items', (done) => {
+  
+  describe('/GET category', () => {
+    it('it should GET all the category items', (done) => {
       chai.request(app)
-        .get('/api/budget')
+        .get('/api/category')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('array');
           res.body.length.should.be.eql(0);
           done();
         });
-      });
     });
+  });
+  
   describe('/POST budget', () => {
-    it('it should POST a value budget item', (done) => {
-      const budget = {
+    it('it should POST a category item', (done) => {
+      const category = {
         name: 'Rent',
         type: 'VALUE',
         amount: 500,
         period: 'MONTHLY'
       };
       chai.request(app)
-        .post('/api/budget')
-        .send(budget)
+        .post('/api/category')
+        .send(category)
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
@@ -40,66 +42,46 @@ describe('Budget', () => {
           res.body.should.have.property('type');
           res.body.should.have.property('amount');
           res.body.should.have.property('period');
-        done();
-      });
+          done();
+        });
     });
-  it('it should POST a percentage budget item', (done) => {
-    const budget = {
-      name: 'Savings',
-      type: 'PERCENT',
-      amount: 10,
-    };
-    chai.request(app)
-      .post('/api/budget')
-      .send(budget)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.property('name');
-        res.body.should.have.property('type');
-        res.body.should.have.property('amount');
-        done();
-      });
+  });
+  
+  describe('/PUT/:id category', () => {
+    it('it should UPDATE a category item given the id', (done) => {
+      const category = {
+        name: 'Savings',
+        type: 'VALUE',
+        amount: 10,
+        period: 'WEEKLY' 
+      }
+      db.category.addItem(category).then((err, budget) => {
+        chai.request(app)
+          .put('/api/category/' + category._id)
+          .send({name: 'Spending', type: 'PERCENT', amount: 10})
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.book.should.have.property('name').eql('Spending');
+            res.body.book.should.have.property('type').eql('PERCENT');
+            res.body.book.should.have.property('amount').eql(10);
+            done();
+          });
+      })
+      .catch(done);
     });
-    // TODO: Category testing
   });
-  describe('/PUT/:id budget', () => {
-      it('it should UPDATE a budget item given the id', (done) => {
-        const budget = {
-          name: 'Savings',
-          type: 'VALUE',
-          amount: 10,
-          period: 'WEEKLY' 
-        }
-        db.budget.addItem(budget).then((err, budget) => {
-          chai.request(app)
-            .put('/api/budget/' + budget._id)
-            .send({name: 'Spending', type: 'PERCENT', amount: 10})
-            .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.be.a('object');
-              res.body.book.should.have.property('name').eql('Spending');
-              res.body.book.should.have.property('type').eql('PERCENT');
-              res.body.book.should.have.property('amount').eql(10);
-              done();
-            });
-        })
-        .catch(done);
-      });
-  });
- /*
-  * Test the /DELETE/:id route
-  */
-  describe('/DELETE/:id book', () => {
-    it('it should DELETE a book given the id', (done) => {
-      const budget = {
+  
+  describe('/DELETE/:id category', () => {
+    it('it should DELETE a category given the id', (done) => {
+      const category = {
         name: "Savings",
         type: VALUE,
         amount: 10,
       }
-      db.budget.addItem(budget).then((err, budget) => {
+      db.category.addItem(category).then((err, budget) => {
         chai.request(app)
-          .delete('/api/budget/' + budget._id)
+          .delete('/api/category/' + category._id)
           .end((err, res) => {
             res.should.have.status(200);
             res.body.should.be.a('number');
@@ -107,6 +89,7 @@ describe('Budget', () => {
             done();
           });
         });
-      });
+      })
+      .catch(done);
   });
 });
