bf578d6340e5ba2a52e83999317907add3951a90
Ryan Baxley <rynobax@users.noreply.github.com>
2017-05-11 10:28:35 -0400

    Create categoryController.js

---
+++ api/budget/categoryController.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const {category} = require('../../db');
+const {respond} = require('../response');
+
+router.post('/', function (req, res) {
+  respond(res, category.addItem(req.body.item));
+});
+
+router.get('/', function (req, res) {
+  respond(res, category.getItems());
+});
+
+router.put('/:id', function (req, res) {
+  respond(res, category.updateItem(req.params.id, req.body.item));
+});
+
+router.delete('/:id', function (req, res) {
+  respond(res, category.deleteItem(req.params.id));
+});
+
+module.exports.CategoryController = router;
