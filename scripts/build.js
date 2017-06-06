438d49d52aaf1125eb0330ad0a337cdb13e1413f
Ryan Baxley <rbaxley@sourcelink.com>
2017-06-06 14:24:45 -0400

    Fixing paths in build

--- scripts/build.js
+++ scripts/build.js
@@ -7,7 +7,7 @@ const rimraf = require('rimraf');
 function clone(){
   console.log('Cloning source files');
   return new Promise((resolve, reject) => {
-    ncp('./src', './tmp', function (err) {
+    ncp(path.join(__dirname, '..', 'src'), path.join(__dirname, '..', 'tmp'), function (err) {
       if (err) {
         console.error(err);
         process.exit(1);
@@ -19,33 +19,35 @@ function clone(){
 
 function changeConfig(){
   console.log('Updating config');
-  // Grabs vars from Travis and populates config file
-  if(!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !process.env.AWS_REGION){
-    console.log('Missing an envirmental variable');
-    process.exit(1);
-  }
   return new Promise((resolve, reject) => {
-    replace({
-      regex: "accessKeyId: 'AKID'",
-      replacement: "accessKeyId: '" + process.env.AWS_ACCESS_KEY_ID + "'",
-      paths: ['./tmp'],
-      recursive: true,
-      silent: true,
-    });
-    replace({
-      regex: "secretAccessKey: 'SECRET'",
-      replacement: "secretAccessKey: '" + process.env.AWS_SECRET_ACCESS_KEY + "'",
-      paths: ['./tmp'],
-      recursive: true,
-      silent: true,
-    });
-    replace({
-      regex: "region: 'us-east-1'",
-      replacement: "region: '" + process.env.AWS_REGION + "'",
-      paths: ['./tmp'],
-      recursive: true,
-      silent: true,
-    });
+    // Grabs vars from Travis and populates config file
+    if(process.env.travis === true){
+      if(!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !process.env.AWS_REGION){
+        console.log('Missing an envirmental variable');
+        process.exit(1);
+      }
+      replace({
+        regex: "accessKeyId: 'AKID'",
+        replacement: "accessKeyId: '" + process.env.AWS_ACCESS_KEY_ID + "'",
+        paths: [path.join(__dirname, '..', 'tmp')],
+        recursive: true,
+        silent: true,
+      });
+      replace({
+        regex: "secretAccessKey: 'SECRET'",
+        replacement: "secretAccessKey: '" + process.env.AWS_SECRET_ACCESS_KEY + "'",
+        paths: [path.join(__dirname, '..', 'tmp')],
+        recursive: true,
+        silent: true,
+      });
+      replace({
+        regex: "region: 'us-east-1'",
+        replacement: "region: '" + process.env.AWS_REGION + "'",
+        paths: [path.join(__dirname, '..', 'tmp')],
+        recursive: true,
+        silent: true,
+      });
+    }
     resolve();
   });
 }
@@ -55,10 +57,10 @@ function pack(){
   return new Promise((resolve, reject) => {
     // returns a Compiler instance
     webpack({
-      entry: './tmp/lambda.js',
+      entry: path.join(__dirname, '..', 'tmp', 'lambda.js'),
       target: 'node',
       output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, '..', 'dist'),
         filename: 'lambda.js'
       }
     }, (err, stats) => {
@@ -74,7 +76,7 @@ function pack(){
 function deleteTmp(){
   console.log('Deleting tmp dir');
   return new Promise((resolve, reject) => {
-    rimraf('./tmp', [], (err) => {
+    rimraf(path.join(__dirname, '..', 'tmp'), [], (err) => {
       if (err) {
         console.error(err);
         process.exit(1);
