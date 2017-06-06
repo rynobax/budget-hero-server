ded08632592ac6d57e3a9d7d76145501cb4cc21a
Ryan Baxley <rbaxley@sourcelink.com>
2017-06-06 10:36:29 -0400

    Switching to bcryptjs

--- src/db/userDB.js
+++ src/db/userDB.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 module.exports = function(dynamoose, DBVersion){
   const session = require('./sessionDB')(dynamoose, DBVersion);
