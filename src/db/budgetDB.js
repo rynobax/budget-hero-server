6fd00a57dfc227b6a8b39108e2e86e1e98fc04a0
Ryan Baxley <rbaxley@sourcelink.com>
2017-06-12 15:06:29 -0400

    More logs for aws

--- src/db/budgetDB.js
+++ src/db/budgetDB.js
@@ -132,8 +132,10 @@ module.exports = function(dynamoose, DBVersion){
   }
 
   function deleteItem(username, id){
+    console.log('deleting username: ' + username + ', id: ' + id)
     return new Promise((resolve, reject) => {
       BudgetItem.delete({username: username, id: id}, (err) => {
+        console.log('err: ', err);
         if(err) resolve({deleted: false, error: err});
         else resolve({deleted: true});
       });
