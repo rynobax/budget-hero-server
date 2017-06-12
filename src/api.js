41cfb96cfd05b1315125a9858c1eafa62e25d1d0
Ryan Baxley <rbaxley@sourcelink.com>
2017-06-12 14:55:47 -0400

    Log for aws test

--- src/api.js
+++ src/api.js
@@ -21,6 +21,7 @@ router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
+  console.log('cors was set');
   next();
 });
 
